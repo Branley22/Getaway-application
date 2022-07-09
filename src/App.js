35b1bf8c7919426cba15696bf89a734dbfd731e0
Branley Minaya <bminaya23@gmail.com>
2022-07-09 16:47:38 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Trips from './pages/Trips';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Stories from './pages/Stories';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar/>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/trips" exact component={Trips} />
-          <Route path="/about" exact component={About} />
-          <Route path="/contact" exact component={Contact} />
-          <Route path="/stories" exact component={Stories} />
-        </Switch>
-        <Footer/>
-     </Router>
+      <Navbar />
     </div>
   );
 }
 
 export default App;
- 

6804eb81faef68ba38adb7a01843f9a96c6d0a79
Branley Minaya <bminaya23@gmail.com>
2022-07-16 07:48:04 -0400

    carousel npm

--- src/components/slideShow/SlideShow.js
+++ src/components/slideShow/SlideShow.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import './SlideShow.css'
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import BoraBora from '../../assets/bora-bora.jpg'
+import BoraBora2 from '../../assets/bora-bora2.jpg'
+import Hawaii from '../../assets/hawaii.jpg'
 
 const SlideShow = () => {
   return (
-    <div>
-
+    <div className='container'>
+      <Carousel className='slideshow' showArrows={true} infiniteLoop={true} autoPlay={true}>
+        <div>
+          <img src={BoraBora} alt='/' />
+        </div>
+        <div>
+          <img src={BoraBora2} alt='/' />
+        </div>
+        <div>
+          <img src={Hawaii} alt='/' />
+        </div>
+      </Carousel>
     </div>
   )
 }
